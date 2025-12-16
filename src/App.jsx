d306415dc674ddb7757e6abf90e3a63d062b6952
primepicks <fxvault9@gmail.com>
2025-12-16 20:54:22 +0300

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,18 @@
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, Navigate } from "react-router-dom";
 import Login from "./pages/Login";
-import AdminDashboard from "./pages/AdminDashboard";
 import MemberDashboard from "./pages/MemberDashboard";
-import ProtectedRoute from "./components/common/ProtectedRoute";
+import AdminDashboard from "./pages/AdminDashboard";
 
-export default function App() {
+function App() {
   return (
     <Routes>
-      <Route path="/" element={<Login />} />
-
-      <Route
-        path="/admin"
-        element={
-          <ProtectedRoute role="admin">
-            <AdminDashboard />
-          </ProtectedRoute>
-        }
-      />
-
-      <Route
-        path="/member"
-        element={
-          <ProtectedRoute role="member">
-            <MemberDashboard />
-          </ProtectedRoute>
-        }
-      />
+      <Route path="/login" element={<Login />} />
+      <Route path="/member-dashboard" element={<MemberDashboard />} />
+      <Route path="/admin-dashboard" element={<AdminDashboard />} />
+      {/* Redirect unknown paths to login */}
+      <Route path="*" element={<Navigate to="/login" replace />} />
     </Routes>
   );
 }
+
+export default App;
